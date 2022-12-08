a8fb1b0e428798cdeaa9cc12f5f723823fda412a
Aiko <46852063+ItsJustAiko@users.noreply.github.com>
2022-12-08 13:30:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import time
 
+asciiCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2"]
+
 def current_milli_time():
     return round(time.time() * 1000)
 
 def encode(phraseAEncoder):
     date = str(current_milli_time())
-    arr = list(date)[-6:]
+    dateArr = list(date)[-6:]
+    strArray = list(phraseAEncoder)
+
+    for i in range(strArray):
+        letter = strArray[i]
+        number = dateArr[i]
+        index = asciiCharacters.index(letter) + number;
 
 
 encode()
