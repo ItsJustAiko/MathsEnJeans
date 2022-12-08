dd1c6526d83c2686846ce69e1493d6639ea8ee36
Aiko <46852063+ItsJustAiko@users.noreply.github.com>
2022-12-08 13:25:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import time
 def current_milli_time():
     return round(time.time() * 1000)
 
-def encode():
+def encode(phraseAEncoder):
     date = str(current_milli_time())
     arr = list(date)[-6:]
-    print(arr)
-    print(date)
+
 
 encode()
